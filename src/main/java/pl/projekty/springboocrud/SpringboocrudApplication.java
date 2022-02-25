e519ed116e427dd6921074126e9489da0d4c8ad0
kaszakinga90 <kaszakinga90@gmail.com>
2022-02-25 12:00:03 +0100

    first commit

---
+++ src/main/java/pl/projekty/springboocrud/SpringboocrudApplication.java
@@ -0,0 +1,13 @@
+package pl.projekty.springboocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboocrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboocrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/projekty/springboocrud/SpringboocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekty.springboocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboocrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
